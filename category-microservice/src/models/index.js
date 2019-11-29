748350e5847897067a5001c1013ce32f7275bb8e
Hao HT <haoht@gviet.vn>
2019-11-29 14:26:00 +0700

    feat(models)!: Thêm func validate category

---
+++ category-microservice/src/models/index.js
@@ -0,0 +1,19 @@
+const joi = require('joi')
+const category = require('./category.model')(joi)
+const schemas = { category }
+const schemaValidator = (object, type) => {
+  return new Promise((resolve, reject) => {
+    if (!object) {
+      reject(new Error('object to validate not provided'))
+    }
+    if (!type) {
+      reject(new Error('schema type to validate not provided'))
+    }
+    const { error, value } = joi.validate(object, schemas[type])
+    if (error) {
+      reject(new Error(`invalid ${type} data, err: ${error}`))
+    }
+    resolve(value)
+  })
+}
+module.exports = { schemas, schemaValidator }
