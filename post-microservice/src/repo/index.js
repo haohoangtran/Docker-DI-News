6d2c42b062c2d5c5f9233e95dd9238377e38caaf
Hao HT <haoht@gviet.vn>
2019-11-29 15:47:36 +0700

    fix(repo)!: change file ref...

--- post-microservice/src/repo/index.js
+++ post-microservice/src/repo/index.js
@@ -1,5 +1,5 @@
 const repo = (mongoClient, options) => {
-  return require('./categoryRepo')(mongoClient, options)
+  return require('./postRepo')(mongoClient, options)
 }
 const connect = (dbPool, options) => {
   if (!dbPool) throw new Error('Connect DB failed')
