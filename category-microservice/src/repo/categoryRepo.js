df1f5b94cd3cfbe067fc5f8b358813cb34c7151c
Hao HT <haoht@gviet.vn>
2019-11-29 14:42:45 +0700

    chore(repo)!: sửa error throw, refactor function

--- category-microservice/src/repo/categoryRepo.js
+++ category-microservice/src/repo/categoryRepo.js
@@ -6,25 +6,33 @@ module.exports = function (mongoClient, options) {
   }
 
   createIndex()
-  const addCategory = (catagory) => {
+  const addCategory = (category) => {
     return new Promise((resolve, reject) => {
-      catagory.createBy = new ObjectId(catagory.createBy)
-      mongoClient.collection('categories').insertOne(catagory, (err, data) => {
-        err ? reject(err) : resolve(data)
+      category.createBy = new ObjectId(category.createBy)
+      mongoClient.collection('categories').insertOne(category, (err, data) => {
+        if (err) reject(new Error(err))
+        else {
+          const { result, ops } = data
+          if (result.ok) {
+            resolve(ops)
+          } else {
+            reject(new Error('Không thể thêm category.'))
+          }
+        }
       })
     })
   }
   const getCategoryById = (id) => {
     return new Promise((resolve, reject) => {
       mongoClient.collection('categories').findOne({ _id: new ObjectId(id) }, (err, data) => {
-        err ? reject(err) : resolve(data)
+        err ? reject(new Error(err)) : resolve(data)
       })
     })
   }
   const deleteCategory = (id) => {
     return new Promise((resolve, reject) => {
       mongoClient.collection('categories').remove({ _id: new ObjectId(id) }, (err, data) => {
-        err ? reject(err) : resolve(data)
+        err ? reject(new Error(err)) : resolve('ok')
       })
     })
   }
