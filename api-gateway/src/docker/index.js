d19f84c86d67849d0555da572adc13d2031e5756
Hao HT <haoht@gviet.vn>
2019-12-02 13:42:32 +0700

    refactor(config): Doi thu muc config  ,

--- api-gateway/src/docker/index.js
+++ api-gateway/src/docker/index.js
@@ -40,26 +40,4 @@ const discoverRoutes = (container) => {
   })
 }
 
-function test () {
-  return new Promise((resolve, reject) => {
-    resolve({
-      1: {
-        id: 1,
-        route: '/api/v1/user',
-        target: 'http://192.168.2.17:3001'
-      },
-      2: {
-        id: 2,
-        route: '/api/v1/post',
-        target: 'http://192.168.2.17:3003'
-      },
-      3: {
-        id: 3,
-        route: '/api/v1/category',
-        target: 'http://192.168.2.17:3002'
-      }
-    })
-  })
-}
-
-module.exports = { discoverRoutes:test }
+module.exports = { discoverRoutes }
